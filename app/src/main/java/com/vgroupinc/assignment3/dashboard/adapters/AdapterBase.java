0d4cbacba514190cd36d62bac7d2d9d7144d30b7
rohitsinhavgroup <aman.anandx3@gmail.com>
2017-12-20 12:39:22 +0530

    commit @ 20/12/17

---
+++ app/src/main/java/com/vgroupinc/assignment3/dashboard/adapters/AdapterBase.java
@@ -0,0 +1,14 @@
+package com.vgroupinc.assignment3.dashboard.adapters;
+
+import android.widget.BaseAdapter;
+
+import com.vgroupinc.assignment3.R;
+
+/**
+ * Created by DELL on 12/18/2017.
+ */
+
+public abstract class AdapterBase extends BaseAdapter {
+    public int[] color_array = new int[]{R.color.a, R.color.b, R.color.c, R.color.d, R.color.e, R.color.f, R.color.g, R.color.h, R.color.i, R.color.j, R.color.k, R.color.l};
+
+}
---
+++ app/src/main/java/com/vgroupinc/assignment3/picasso/PicassoImp.java
@@ -0,0 +1,120 @@
+package com.vgroupinc.assignment3.picasso;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.text.TextUtils;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+import com.vgroupinc.assignment3.R;
+import com.vgroupinc.assignment3.utils.Utils;
+
+/**
+ * Created by DELL on 12/18/2017.
+ */
+
+public class PicassoImp {
+    private static final PicassoImp ourInstance = new PicassoImp();
+
+    private PicassoImp() {
+    }
+
+    public static PicassoImp getInstance() {
+
+        return ourInstance;
+    }
+
+    public void setImage(Object imageKey, final Context context, int position, final ImageView imageView) {
+        int color = Utils.color_array[Utils.getColorPosition(position)];
+
+        if (imageKey != null && !TextUtils.isEmpty(String.valueOf(imageKey))) {
+            Picasso.with(context)
+                    .load(String.valueOf(imageKey))
+                    .error(color)
+                    .placeholder(color)
+                    .into(imageView);
+        } else {
+            Picasso.with(context)
+                    .load(color)
+                    .error(color)
+                    .placeholder(color)
+                    .into(imageView);
+        }
+
+    }
+
+    /**
+     * @param hypable   true for tournament, false for events
+     * @param imageKey
+     * @param context
+     * @param position
+     * @param imageView
+     */
+    public void setImageBackground(String hypable, Object imageKey, final Context context, int position, final ImageView imageView) {
+        int color = Utils.color_array[Utils.getColorPosition(position)];
+        if (imageKey != null && !TextUtils.isEmpty(String.valueOf(imageKey))) {
+            Picasso.with(context)
+                    .load(String.valueOf(imageKey))
+                    .error(color)
+                    .placeholder(color)
+                    .into(new Target() {
+                        @Override
+                        public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                            imageView.setBackground(new BitmapDrawable(context.getResources(), bitmap));
+                        }
+
+                        @Override
+                        public void onBitmapFailed(Drawable errorDrawable) {
+                            imageView.setBackground(errorDrawable);
+                        }
+
+                        @Override
+                        public void onPrepareLoad(Drawable placeHolderDrawable) {
+                            imageView.setBackground(placeHolderDrawable);
+                        }
+                    });
+        } else {
+            Picasso.with(context)
+                    .load(color)
+                    .error(color)
+                    .placeholder(color)
+                    .into(new Target() {
+                        @Override
+                        public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                            imageView.setBackground(new BitmapDrawable(context.getResources(), bitmap));
+                        }
+
+                        @Override
+                        public void onBitmapFailed(Drawable errorDrawable) {
+                            imageView.setBackground(errorDrawable);
+                        }
+
+                        @Override
+                        public void onPrepareLoad(Drawable placeHolderDrawable) {
+                            imageView.setBackground(placeHolderDrawable);
+                        }
+                    });
+            imageView.setAlpha(0.8f);
+        }
+        if (hypable.equals("TOURNAMENT")) {
+            //for tournament
+            imageView.setImageResource(R.drawable.hype_border);
+        } else {
+            //for event
+            imageView.setImageResource(R.drawable.hype_border_blue);
+        }
+
+    }
+
+    public void setImage(String url, Activity activity, ImageView imageView, int image) {
+        Picasso.with(activity)
+                .load(url)
+                .placeholder(image)
+                .error(image)
+                .into(imageView);
+    }
+}
